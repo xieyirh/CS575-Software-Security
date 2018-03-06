6f0fcabb609d9bdecdeffddae0c274eb207a15c8
Seed <seed@ubuntu.(none)>
2018-03-06 15:04:56 -0800

    downloaded labfiles from seedsLab

---
+++ SeedsLab/sample.c
@@ -0,0 +1,8 @@
+
+void func(int a, int b){
+	int x, y;
+	x = a + b;
+	y = a - b;
+}
+
+
---
+++ SeedsLab/sample.s
@@ -0,0 +1,32 @@
+	.file	"sample.c"
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	12(%ebp), %eax
+	movl	8(%ebp), %edx
+	addl	%edx, %eax
+	movl	%eax, -8(%ebp)
+	movl	12(%ebp), %eax
+	movl	8(%ebp), %edx
+	movl	%edx, %ecx
+	subl	%eax, %ecx
+	movl	%ecx, %eax
+	movl	%eax, -4(%ebp)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.ident	"GCC: (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3"
+	.section	.note.GNU-stack,"",@progbits
